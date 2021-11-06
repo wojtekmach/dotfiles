256e766a53bcb8d6f61d62210020863e20d201b7
Wojtek Mach <wojtek@wojtekmach.pl>
2021-11-06 10:46:05 +0100

    up

--- elixir/.iex.exs
+++ elixir/.iex.exs
@@ -1,10 +1,35 @@
-if System.get_env("M") && :mix not in Enum.map(Application.loaded_applications(), &elem(&1, 0)) do
+if :mix not in Enum.map(Application.loaded_applications(), &elem(&1, 0)) do
   Mix.install([
-    :decimal,
-    {:req, path: "~/src/req"},
-    {:system_castore, path: "~/src/system_castore"},
-    {:easyxml, path: "~/src/easyxml", override: true},
-    :saxmerl,
-    :floki
+    # :decimal,
+    # {:req, path: "~/src/req"},
+    # {:system_castore, path: "~/src/system_castore"},
+    # {:easyxml, path: "~/src/easyxml", override: true},
+    # :saxmerl,
+    # :floki,
+    {:sigils, path: "~/src/sigils"}
   ])
 end
+
+defmodule MyInspect do
+  import Inspect.Algebra
+
+  def inspect(%URI{} = url, _opts) do
+    concat(["~URI[", URI.to_string(url), "]"])
+  end
+
+  def inspect(%Date.Range{step: 1} = range, _opts) do
+    "~Date.Range[#{range.first}..#{range.last}]"
+  end
+
+  def inspect(%Date.Range{} = range, _opts) do
+    "~Date.Range[#{range.first}..#{range.last}//#{range.step}]"
+  end
+
+  def inspect(term, opts) do
+    Inspect.inspect(term, opts)
+  end
+end
+
+Inspect.Opts.default_inspect_fun(&MyInspect.inspect/2)
+
+# import Sigils
